3f5c5f582bd1992044d3c00ff5b29b5698febaa6
José Teste <teste14.teste14@hotmail.com>
2019-04-24 20:12:41 -0300

    Funcionando

--- CalculadoraVisual/Form1.cs
+++ CalculadoraVisual/Form1.cs
@@ -16,5 +16,30 @@ namespace CalculadoraVisual
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float A = int.Parse(textBox1.Text);
+            float B = int.Parse(textBox2.Text);
+
+            float R = 0;
+
+            switch (comboBox1.Text)
+            {
+                case "Adição":
+                    R = A + B;
+                    break;
+                case "Subtração":
+                    R = A - B;
+                    break;
+                case "Multiplicação":
+                    R = A * B;
+                    break;
+                case "Divisão":
+                    R = A / B;
+                    break;
+            }
+            MessageBox.Show(R.ToString());
+        }
     }
 }
